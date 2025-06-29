62562185c9060eafdc35b9b6d3bc6aad1e252d01
furahaderick <yvesderricks@gmail.com>
2025-06-29 20:18:48 +0200

    Create server with app.js and server.js

---
+++ src/app.js
@@ -0,0 +1,21 @@
+import express from "express";
+
+import authRouter from "./routes/auth.router.js";
+
+const app = express();
+
+// App-level Middleware
+app.use(express.json());
+
+app.get("/", (req, res) => {
+	res.status(200).send("Server Up And Running");
+});
+
+app.use("/auth", authRouter);
+
+app.use((err, req, res, next) => {
+	console.error(err.stack);
+	res.status(500).send("Internal Server Error");
+});
+
+export default app;
--- src/server.js
+++ src/server.js
@@ -1,1 +1,13 @@
-console.log("Backend Service for a Blog Application");
+import http from "node:http";
+
+import { startDB } from "./config/database.config.js";
+
+import app from "./app.js";
+const PORT = process.env.PORT || 5000;
+
+const server = http.createServer(app);
+
+server.listen(PORT, () => {
+	startDB();
+	console.log(`Server Running on *:${PORT}`);
+});

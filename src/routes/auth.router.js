003600c8bd5bb5671fec40b0f6e009fedc561207
furahaderick <yvesderricks@gmail.com>
2025-07-01 14:24:01 +0200

    Login route

--- src/routes/auth.router.js
+++ src/routes/auth.router.js
@@ -1,10 +1,14 @@
 import express from "express";
 
-import { register } from "../controllers/auth.controller.js";
-import { registerValidator } from "../middleware/validator.middleware.js";
+import { register, login } from "../controllers/auth.controller.js";
+import {
+	registerValidator,
+	loginValidator,
+} from "../middleware/validator.middleware.js";
 
 const authRouter = express.Router();
 
 authRouter.post("/register", registerValidator, register);
+authRouter.post("/login", loginValidator, login);
 
 export default authRouter;

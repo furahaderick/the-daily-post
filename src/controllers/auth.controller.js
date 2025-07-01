243e439619edea8f1b10f385cb4bcf5ef686e95f
furahaderick <yvesderricks@gmail.com>
2025-07-01 14:23:33 +0200

    Login logic

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -4,6 +4,8 @@ import bcrypt from "bcryptjs";
 
 import User from "../models/user.model.js";
 
+import { generateTokenAndSetCookie } from "../lib/utils/token.utils.js";
+
 export const register = expressAsyncHandler(async (req, res) => {
 	// Check for any validation errors
 	const errors = validationResult(req);
@@ -38,4 +40,34 @@ export const register = expressAsyncHandler(async (req, res) => {
 	});
 
 	res.status(201).json({ message: "User registered successfully!" });
+});
+
+export const login = expressAsyncHandler(async (req, res) => {
+	// Check for any validation errors
+	const errors = validationResult(req);
+	if (!errors.isEmpty()) {
+		return res.status(400).json({ errors: errors.array() });
+	}
+
+	const { email, password } = req.body;
+
+	// Find user by email
+	const existingEmail = await User.findOne({ email });
+	if (!existingEmail) {
+		return res.status(404).json({ message: "User not found" });
+	}
+
+	// Compare passwords
+	const matchingPasswords = await bcrypt.compare(
+		password,
+		existingEmail.password
+	);
+	if (!matchingPasswords) {
+		return res.status(401).json({ message: "Invalid credentials" });
+	}
+
+	// Generate token and set cookie
+	generateTokenAndSetCookie(existingEmail, res);
+
+	res.status(200).json({ message: "Logged in successfully!" });
 });

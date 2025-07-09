dcc9d535051f9c80924f52c25f0a430f3b7856ab
furahaderick <yvesderricks@gmail.com>
2025-07-09 18:21:01 +0200

    Handle password-reset routes

--- src/routes/auth.router.js
+++ src/routes/auth.router.js
@@ -1,14 +1,32 @@
 import express from "express";
 
-import { register, login } from "../controllers/auth.controller.js";
 import {
-	registerValidator,
-	loginValidator,
+    register,
+    login,
+    requestPasswordResetLink,
+    resetPassword,
+} from "../controllers/auth.controller.js";
+import {
+    registerValidator,
+    loginValidator,
+    passwordResetLinkValidator,
+    resetPasswordValidator,
 } from "../middleware/validator.middleware.js";
 
 const authRouter = express.Router();
 
 authRouter.post("/register", registerValidator, register);
 authRouter.post("/login", loginValidator, login);
+
+authRouter.post(
+	"/forgot-password",
+	passwordResetLinkValidator,
+	requestPasswordResetLink
+);
+authRouter.post(
+	"/reset-password/:userId/:token",
+	resetPasswordValidator,
+	resetPassword
+);
 
 export default authRouter;

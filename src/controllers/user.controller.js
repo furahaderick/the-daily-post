f6b98cf8764131572fca50cbac708dd6b7daa335
furahaderick <yvesderricks@gmail.com>
2025-07-10 18:10:25 +0200

    Update + Retrieve profile logic

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,3 +1,8 @@
+import expressAsyncHandler from "express-async-handler";
+import { validationResult } from "express-validator";
+
+import User from "../models/user.model.js";
+
 export const getLoggedInUser = (req, res) => {
 	// Check if user is authenticated
 	if (!req.user) {
@@ -6,3 +11,38 @@ export const getLoggedInUser = (req, res) => {
 
 	return res.status(200).json(req.user);
 };
+
+export const updateLoggedInUser = expressAsyncHandler(async (req, res) => {
+	if (!req.user) {
+		return res.status(401).json({ message: "Unauthorized" });
+	}
+
+	// Check for any validation errors
+	const errors = validationResult(req);
+	if (!errors.isEmpty()) {
+		return res.status(400).json({ errors: errors.array() });
+	}
+
+	// Update any field the user might want to change without mandatory restrictions
+	const updates = req.body;
+
+	const allowedFields = ["fullName", "username", "email"];
+	const updateData = {};
+
+	allowedFields.forEach((field) => {
+		if (updates[field] !== undefined) {
+			updateData[field] = updates[field];
+		}
+	});
+
+	// Update user
+	const updatedUser = await User.findByIdAndUpdate(req.user.id, updateData, {
+		new: true,
+	});
+
+	if (!updatedUser) {
+		return res.status(404).json({ message: "Profile update failed" });
+	}
+
+	res.status(200).json({ message: "Profile updated successfully!" });
+});

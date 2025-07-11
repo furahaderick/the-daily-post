92ca5f07016a34a67f32fcef06c9f60aceb7db83
furahaderick <yvesderricks@gmail.com>
2025-07-12 00:46:52 +0200

    Add email verification prop to user model

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -5,7 +5,12 @@ const userSchema = new mongoose.Schema({
 	username: { type: String, required: true, unique: true },
 	email: { type: String, required: true, unique: true },
 	password: { type: String, required: true },
-	role: { type: String, enum: ["admin", "author", "reader"], default: "reader" },
+	role: {
+		type: String,
+		enum: ["admin", "author", "reader"],
+		default: "reader",
+	},
+	verified: { type: Boolean, default: false },
 });
 
 const User = mongoose.model("User", userSchema);

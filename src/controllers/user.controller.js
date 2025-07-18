9fcf4546c1f566e849a6ae96fdc5db4f58791527
furahaderick <yvesderricks@gmail.com>
2025-07-18 12:44:00 +0200

    Change password while authenticated

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,5 +1,6 @@
 import expressAsyncHandler from "express-async-handler";
 import { validationResult } from "express-validator";
+import bcrypt from "bcryptjs";
 
 import User from "../models/user.model.js";
 
@@ -45,4 +46,42 @@ export const updateLoggedInUser = expressAsyncHandler(async (req, res) => {
 	}
 
 	res.status(200).json({ message: "Profile updated successfully!" });
+});
+
+export const changePassword = expressAsyncHandler(async (req, res) => {
+    // Check for any validation errors
+	const errors = validationResult(req);
+	if (!errors.isEmpty()) {
+		return res.status(400).json({ errors: errors.array() });
+	}
+    
+	const { currentPassword, newPassword, confirmNewPassword } = req.body;
+	const userId = req.user?._id;
+
+	const user = await User.findById(userId);
+	if (!user) {
+		return res.status(404).json({ message: "User not found" });
+	}
+
+	const passwordMatch = await bcrypt.compare(currentPassword, user.password);
+	if (!passwordMatch) {
+		return res.status(401).json({ message: "Incorrect current password." });
+	}
+
+	// Hash new password
+	const salt = await bcrypt.genSalt(10);
+	const hashedPassword = await bcrypt.hash(newPassword, salt);
+
+	user.password = hashedPassword;
+	await user.save();
+
+	// Invalidate session
+	res.cookie("access_token", "", {
+		httpOnly: true,
+		secure: process.env.NODE_ENV === "production",
+		expires: new Date(0), // Set to expire immediately
+		sameSite: "strict",
+	});
+
+	res.status(200).json({ message: "Password changed successfully." });
 });
